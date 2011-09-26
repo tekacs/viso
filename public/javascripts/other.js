cb20a5c4032b0628e2b6859b75f405bf97e8d5e6
Larry Marburger <larry@marburger.cc>
2011-09-26 17:17:20 -0400

    Remove unnecessary closure that breaks when minified

--- public/javascripts/other.js
+++ public/javascripts/other.js
@@ -1,46 +1,44 @@
-(function($) {
-  $(function() {
-    if (!$("body#other").length) { return; }
+$(function() {
+  if (!$("body#other").length) { return; }
 
-    var viewport = $(window),
-        content  = $("#content"),
-        wrapper  = content.closest(".wrapper"),
-        link     = content.find("a");
+  var viewport = $(window),
+      content  = $("#content"),
+      wrapper  = content.closest(".wrapper"),
+      link     = content.find("a");
 
-    content
+  content
 
-      // Center the download box vertically within the viewport accounting for
-      // the padding around the body.
-      .bind("center", function() {
-        var viewportSize = viewport.height() - wrapper.height() - 36;
+    // Center the download box vertically within the viewport accounting for
+    // the padding around the body.
+    .bind("center", function() {
+      var viewportSize = viewport.height() - wrapper.height() - 36;
 
-        var top = Math.floor(viewportSize / 2);
-        wrapper.css({ marginTop: top });
-      })
+      var top = Math.floor(viewportSize / 2);
+      wrapper.css({ marginTop: top });
+    })
 
-      // Trigger `"center"` to kick things off.
-      .trigger("center");
+    // Trigger `"center"` to kick things off.
+    .trigger("center");
 
-    // Recenter the content when the browser is resized.
-    viewport.resize(function() {
-      content.trigger("center");
-    });
-
-    // Show the download button when holding only the option modifier key.
-    var altKeyHandler = function(e) {
-                          if ( e.altKey  &&
-                              !e.ctrlKey &&
-                              !e.metaKey &&
-                              !e.shiftKey) {
-                            link.addClass("download");
-                          } else {
-                            link.removeClass("download");
-                          }
-                        };
-
-    viewport
-      .keydown(altKeyHandler)
-      .keyup(altKeyHandler);
-
+  // Recenter the content when the browser is resized.
+  viewport.resize(function() {
+    content.trigger("center");
   });
-}(jQuery));
+
+  // Show the download button when holding only the option modifier key.
+  var altKeyHandler = function(e) {
+                        if ( e.altKey  &&
+                            !e.ctrlKey &&
+                            !e.metaKey &&
+                            !e.shiftKey) {
+                          link.addClass("download");
+                        } else {
+                          link.removeClass("download");
+                        }
+                      };
+
+  viewport
+    .keydown(altKeyHandler)
+    .keyup(altKeyHandler);
+
+});
